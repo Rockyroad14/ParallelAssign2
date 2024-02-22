067c388c728d1fc034a1ed87d9848ef1e956779f
Jared Reich <jared@Jareds-Laptop>
2024-02-22 18:39:38 -0500

    finish main2

--- main2.cpp
+++ main2.cpp
@@ -4,22 +4,25 @@
 #include <cstring>
 #include <thread>
 #include <vector>
-#include <future>
 #include <mutex>
 #include <queue>
 #include <chrono>
 #include <condition_variable>
+#include <random>
+
 
 using namespace std;
 
 class Showroom
 {
     private:
+        
         bool isOccupied;
         int numPeople;
-        vector<thread> persons;
+        vector <thread> persons;
         mutex mtx;
         condition_variable condition;
+        
     public:
         Showroom(int people)
         {
@@ -29,24 +32,36 @@ class Showroom
 
         void enterShowroom()
         {
-             
-            //go or not go 
+            random_device rd;
+            mt19937 gen(rd());
+            uniform_int_distribution<> dist(0, 1);
+            bool toGo = dist(gen);
+
+            // Decide if person wants to visit based on randomness.
+            if (toGo)
+            {
+                unique_lock <mutex> locker(mtx);
             
-            unique_lock <mutex> locker(mtx);
-           
-            isOccupied = true;
-            this_thread::sleep_for(chrono::seconds(2));
-            isOccupied = false;
+                isOccupied = true;
+                this_thread::sleep_for(chrono::seconds(2));
+                isOccupied = false;
 
 
-            condition.notify_one();
+                condition.notify_one();
+            }
+            else
+            {
+                return;
+            }
+            
         }
 
+        
         void beginShowroom()
         {
             for(int i = 0; i < numPeople; i++)
             {
-                persons.emplace_back(thread(&enterShowroom,this));
+                persons.emplace_back(thread(&Showroom::enterShowroom, this));
             }
 
             //Randomly decide to que a thread
@@ -57,8 +72,7 @@ class Showroom
             {
                 thread.join();
             }
-        }
-        
+        }        
 };
 
 
