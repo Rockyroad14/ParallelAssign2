e6837b8ffc22acf7b8de53a40543bb6f98f25742
Rockyroad14 <jaredreich12@gmail.com>
2024-02-23 13:10:52 -0500

    Changed main.cpp and updating readme.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# ParallelAssign2
+# Parallel Assignment 2
+## Part 1:
+## Part 2: 
+### How to Run:
+To run the second program associated with the second Prompt. First run the command `g++ main2.cpp` and then run `./a.exe` or `./a.out` depending on the compiler and environemnt.
+### Approach:
+The approach taken on the showroom with the vase is the 1st strategy of 
--- main.cpp
+++ main.cpp
@@ -1,33 +1,97 @@
-#include <iostream>
-#include <fstream>
-#include <cmath>
-#include <cstring>
-#include <thread>
-#include <vector>
-#include <future>
-#include <mutex>
+#include<iostream>
+#include<thread>
+#include<cstdlib>
+#include<mutex>
+#include<vector>
+#include<condition_variable>
 
 using namespace std;
+int currentIndex = 0;
+bool cup_cake = true;
+int count = 0;
+const int N = 10;
 
-class Labyrinth {
-    public:
-        Labyrinth()
+struct guest
+{
+    bool counter=false;
+    bool has_placed=false; 
+};
+guest guests[N];
+vector<thread> threads;
+mutex mtx;
+condition_variable cv;
+
+int count2 = 0;
+void guestAction(int id)
+{
+    while(count != N)
+    {    
+        mtx.lock();
+        if((cup_cake == false) && (id == currentIndex) && (guests[id].has_placed == false) && (currentIndex != 0))
         {
-            this->cupcake = true;
+            cout << "NONCOUNTER GUEST " << id << endl;
+            guests[id].has_placed = true;
+            cup_cake = true;
+            count2 += 1;
+        }
+        else if(cup_cake == true && id == 0 && currentIndex == 0)
+        {
+            cout << "COUNTER GUEST " << id << endl;
+            count += 1;
+            cup_cake = false;
             
         }
-
-    private:
-        bool cupcake;
-
-
-};
-
-int main(void)
+        mtx.unlock();
+    }
+   
+}
+int main()
 {
-    int guests = 0;
-    cout << "Enter amount of guests: ";
-    cin >> guests;
+    cout << "Starting sim...." << endl;
+
+    srand((unsigned) time(NULL));
+
+    //Designating counter guest
+    guests[0].counter = true;
+    guests[0].has_placed = true; //doesn't matter for the counter
+    threads.push_back(thread(guestAction,0));
+
+    //Initialize guests
+    for(int i = 1; i < N; i++)
+    {
+            guests[i].counter = false;
+            guests[i].has_placed = false;
+            threads.push_back(thread(guestAction,i));
+    }
+
+    auto start = chrono::high_resolution_clock::now();
+
+    while(count != N)
+    {
+        //Generate a random number between 0 and N
+        currentIndex = rand() % N;
+        cout<<"index:"<< currentIndex <<endl;
+        cout <<"Guests Gone:" << count << endl; 
+    }
+
+   
 
 
+    for(auto& t : threads)
+        {
+            if(t.joinable())
+            {
+                t.join();
+            }
+        }
+
+    for(int i = 0; i  < N;i++)
+    {
+        cout <<"GUEST" << i << ": hasPlaced " << guests[i].has_placed << endl;
+    }
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start);
+    cout << "Simulation finished in " << duration.count() << " milliseconds." << endl;
+    cout << "All " << count << " guests have been in the labryinth" << endl;
+    return 0;
 }
