9aaa28dacaa20e2632db99d22f16320744ba612d
Jared Reich <ja548470@ucf.edu>
2024-02-23 15:52:36 -0500

    small fix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
-#include<thread>
-#include<cstdlib>
-#include<mutex>
-#include<vector>
-#include<condition_variable>
+#include <iostream>
+#include <thread>
+#include <cstdlib>
+#include <mutex>
+#include <vector>
+#include <condition_variable>
 
 using namespace std;
 int currentIndex = 0;
@@ -58,14 +58,14 @@ int main()
     //Designating counter guest
     guests[0].array[0] = true;
     guests[0].array[1] = true; //doesn't matter for the counter
-    People.push_back(thread(guestAction,0, guests, N));
+    People.push_back(thread(guestAction,0, guests, ref(N));
 
     //Initialize guests
     for(int i = 1; i < N; i++)
     {
             guests[i].array[0] = false;
             guests[i].array[1] = false;
-            People.push_back(thread(guestAction,i, guests, N));
+            People.push_back(thread(guestAction,i, guests, ref(N));
     }
 
     while(count != N)
